f7890befa7c17b5b0e07abd9d68142110b980d98
Lee Magnusson <lee@unhuman.io>
2025-01-07 15:59:31 -0800

    bug fix updates

--- protocol_parser.cpp
+++ protocol_parser.cpp
@@ -9,7 +9,6 @@
 #include "crc.h"
 
 namespace figure {
-
 uint8_t* ProtocolParser::generatePacket(const uint8_t* buffer, const uint32_t buffer_size,
                                         const uint8_t frame_id, uint8_t* gen_packet_size) {
   // construct a packet with the given data bytes + 2 crc bytes + 2 start bytes + 1 frame id + 1
@@ -27,11 +26,36 @@ uint8_t* ProtocolParser::generatePacket(const uint8_t* buffer, const uint32_t bu
   return &gen_packet_buffer_[0];
 }
 
+size_t ProtocolParser::generatePacketNoCopy(uint8_t* dst_buf, const size_t dst_buf_size,
+                                            const uint8_t* src_buf, const size_t src_buf_size,
+                                            uint8_t frame_id) {
+  // construct a packet with the given data bytes + 2 crc bytes + 2 start bytes + 1 frame id + 1
+  // payload length
+  size_t generated_size = src_buf_size + kNumPrePayloadBytes + kNumCrcBytes;
+
+  if (generated_size > dst_buf_size) {
+    // skip generating packet since it won't fit in destination buffer
+    return 0U;
+  }
+
+  dst_buf[0] = kStartByte1;
+  dst_buf[1] = kStartByte2;
+  dst_buf[2] = frame_id;      // frame id
+  dst_buf[3] = src_buf_size;  // payload length
+  std::memcpy(dst_buf + kNumPrePayloadBytes, src_buf, src_buf_size);
+  // Calculate the crc on the packet and overhead bytes
+  uint16_t crc = lib::crc16(dst_buf, src_buf_size + kNumPrePayloadBytes);
+  dst_buf[src_buf_size + kNumPrePayloadBytes] = crc >> 8;        // NOLINT
+  dst_buf[src_buf_size + kNumPrePayloadBytes + 1] = crc & 0xFF;  // NOLINT
+
+  return generated_size;
+}
+
 // NOLINTNEXTLINE(readability-function-cognitive-complexity)
-void ProtocolParser::process(const int32_t latest_idx) {
+void ProtocolParser::process(const uint32_t latest_idx) {
   // Check that the index is within the buffer size
   if (latest_idx < 0 || latest_idx >= buffer_size_) {
-    // TODO(kyle-figure): Track this error
+    stats_.invalid_idx++;
     return;
   }
   tail_ = (latest_idx + 1) % buffer_size_;
@@ -58,10 +82,13 @@ void ProtocolParser::process(const int32_t latest_idx) {
     // Loop through buffer_ beginning at start_index and find the start of a packet
     for (int i = start_index; i < end_index; i++) {
       if (buffer_[i % buffer_size_] != kStartByte1) {
+        if (buffer_[i % buffer_size_] != 0x00) {
+          stats_.invalid_sync_bytes++;
+        }
         if (i + 1 == end_index) {
           // Reached the end of the buffer without finding a start byte.
           // Discard any bytes that have been processed by setting the head_ to tail_.
-          head_ = tail_;
+          head_ = tail_ % buffer_size_;
           return;
         }
 
@@ -73,7 +100,7 @@ void ProtocolParser::process(const int32_t latest_idx) {
       if (i + 3 > end_index) {
         // Buffer does not contain enough bytes to read the payload length.
         // Update the head_ and return.
-        head_ = i;
+        head_ = i % buffer_size_;
         return;
       }
 
@@ -81,7 +108,7 @@ void ProtocolParser::process(const int32_t latest_idx) {
       if (buffer_[(i + 1) % buffer_size_] != kStartByte2) {
         // Incorrect second start byte
         // Continue to search through buffer for start byte from i+2
-        head_ = i + 2;
+        head_ = (i + 2) % buffer_size_;
         bytes_to_process -= 2;
         break;
       }
@@ -123,8 +150,8 @@ void ProtocolParser::process(const int32_t latest_idx) {
       // Check CRC
       if (crc16(packet_buffer_, payload_length + kNumHeaderBytes) != crc) {
         // CRC is incorrect
-        // TODO(kyle-figure): Track this error
         // Update the head_ and break to start searching for the next packet
+        stats_.invalid_crc++;
         start_index = i + 2;
         head_ = start_index % buffer_size_;
         bytes_to_process = end_index - start_index;
@@ -133,10 +160,11 @@ void ProtocolParser::process(const int32_t latest_idx) {
 
       // Call the callback for the packet
       if (callbacks_[frame_id] != nullptr) {
+        stats_.packets_received++;
         callbacks_[frame_id](&packet_buffer_[kNumHeaderBytes], payload_length);
       } else {
         // No callback registered for the packet
-        // TODO(kyle-figure): Track this error as an invalid frame ID
+        stats_.invalid_frame_id++;
       }
 
       // Update the head_ index
@@ -146,6 +174,56 @@ void ProtocolParser::process(const int32_t latest_idx) {
       break;
     }
   }
+}
+
+bool ProtocolParser::processSingleFrame(const uint8_t* buffer, size_t buffer_size) {
+  const size_t kNumHeaderBytes = 4U;
+  const size_t kNumPayloadLenOffset = 3U;
+  const size_t kFrameIdOffset = 2U;
+  const size_t kCrcBytes = 2U;
+
+  // No enough bytes for a complete frame. drop frame
+  if (buffer_size < (kNumHeaderBytes + kCrcBytes)) {
+    return false;
+  }
+
+  // Search for start bytes
+  for (size_t start_index = 0U; start_index < (buffer_size - kNumHeaderBytes - kCrcBytes);
+       start_index++) {
+    if ((buffer[start_index] != kStartByte1) && (buffer[start_index] != 0x00)) {
+      stats_.invalid_sync_bytes++;
+      continue;
+    }
+    if ((buffer[start_index] == kStartByte1) && (buffer[start_index + 1U] == kStartByte2)) {
+      const uint8_t* packet_buffer = &buffer[start_index];
+      size_t payload_length = packet_buffer[kNumPayloadLenOffset];
+
+      if ((payload_length + kNumHeaderBytes + kCrcBytes) > (buffer_size - start_index)) {
+        return false;
+      }
+
+      const size_t kCrcOffset = payload_length + kNumHeaderBytes;
+      uint16_t calculated_crc = lib::crc16(packet_buffer, payload_length + kNumHeaderBytes);
+      uint16_t received_crc =
+          ((uint16_t)packet_buffer[kCrcOffset] << 8) | packet_buffer[kCrcOffset + 1U];
+
+      if (calculated_crc == received_crc) {
+        uint8_t frame_id = packet_buffer[kFrameIdOffset];
+        if ((frame_id < kMaxFrameIds) && (callbacks_[frame_id] != nullptr)) {
+          stats_.packets_received++;
+          callbacks_[frame_id](&packet_buffer[kNumHeaderBytes], payload_length);
+          return true;
+        } else {
+          // No callback registered for the packet
+          stats_.invalid_frame_id++;
+        }
+      } else {
+        stats_.invalid_crc++;
+      }
+      break;
+    }
+  }
+  return false;
 }
 
 void ProtocolParser::registerCallback(uint8_t frame_id, callback_t&& callback) {
--- protocol_parser.h
+++ protocol_parser.h
@@ -10,6 +10,7 @@ namespace figure {
 /// @brief Parse the protocol messages.
 /// https://figure-ai.atlassian.net/l/cp/QGFVFYL9
 class ProtocolParser {
+ public:
   constexpr static uint8_t kStartByte1 = 0xCA;
   constexpr static uint8_t kStartByte2 = 0xFE;
   constexpr static uint8_t kNumStartBytes = 2;
@@ -21,7 +22,6 @@ class ProtocolParser {
   constexpr static uint16_t kMaxPacketSize = 256;
   using callback_t = std::function<void(const uint8_t*, uint16_t)>;
 
- public:
   /// @brief Constructor to parse the protocol messages from a DMA buffer.
   /// @param buffer Pointer to the DMA buffer.
   /// @param buffer_size Size of the DMA buffer.
@@ -30,7 +30,7 @@ class ProtocolParser {
 
   /// @brief Process all new elements in the buffer and call associated callbacks.
   /// @param latest_idx index to the latest element in the buffer.
-  void process(int32_t latest_idx);
+  void process(const uint32_t latest_idx);
 
   /// @brief Register a callback to be called when a packet with the given frame_id is received.
   /// @param frame_id The frame_id to register the callback for.
@@ -46,8 +46,36 @@ class ProtocolParser {
   uint8_t* generatePacket(const uint8_t* buffer, const uint32_t buffer_size, const uint8_t frame_id,
                           uint8_t* gen_packet_size);
 
-  // TODO(kyle): Add getStats function to get the number of packets received, number of packets
-  // dropped, etc.
+  /// @brief Generate a packet that corresponds to the protocol.
+  /// @param dst_buf Pointer to the destination packet buffer.
+  /// @param dst_buf_size Size of the destination packet buffer.
+  /// @param src_buf Pointer to the source packet buffer.
+  /// @param src_buf_size Size of the source packet buffer.
+  /// @param frame_id The frame_id of the generated packet.
+  /// @return Total size of generated packet
+  size_t generatePacketNoCopy(uint8_t* dst_buf, const size_t dst_buf_size, const uint8_t* src_buf,
+                              const size_t src_buf_size, uint8_t frame_id);
+
+  /// @brief Process packet in given buffer
+  /// @param buffer Pointer to the packet buffer
+  /// @param buffer_size Size of the packet buffer
+  /// @return true if a a valid frame is received. false if no
+  bool processSingleFrame(const uint8_t* buffer, size_t buffer_size);
+  struct ProtocolStats {
+    uint32_t invalid_crc;          // CRC errors
+    uint32_t invalid_idx;          // Invalid circular buffer index error
+    uint32_t invalid_frame_id;     // Invalid frame id error
+    uint32_t invalid_sync_bytes;   // Invalid sync byte error
+    uint32_t packets_received;     // Number of packets received
+    uint32_t non_zero_sync_bytes;  // Number of packets with non-zero sync bytes
+  };
+
+  /// @brief Get the stats of the protocol parser
+  /// @return The stats of the protocol parser
+  ProtocolStats getStats() const { return stats_; }
+
+  /// @brief Clear the stats of the protocol parser
+  void clearStats() { stats_ = {}; }
 
  private:
   const uint8_t* buffer_;          // Pointer to the DMA buffer.
@@ -57,6 +85,7 @@ class ProtocolParser {
   callback_t callbacks_[kMaxFrameIds];         // Callbacks to call when a packet is received.
   uint8_t packet_buffer_[kMaxPacketSize];      // Buffer to store the packet being parsed.
   uint8_t gen_packet_buffer_[kMaxPacketSize];  // Buffer to store the packet being generated.
+  ProtocolStats stats_ = {};                   // Stats to keep track of the protocol counters
 };
 
 }  // namespace figure
